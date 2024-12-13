1eb686c40a6ebf26ec4362385cf9d9029544e9f0
Abdullag4 <abdullakawkas4@gmail.com>
2024-12-13 09:10:19 +0300

    Update Setting.py

--- Setting.py
+++ Setting.py
@@ -5,6 +5,10 @@ def show_settings(finance_data, db_file):
     st.header("Settings")
     clear = st.button("Clear All Data")
     if clear:
-        finance_data = pd.DataFrame(columns=["Date", "Type", "Category", "Amount", "Notes"])
-        finance_data.to_csv(db_file, index=False)  # Save the cleared data to CSV
-        st.success("All data cleared!")
+        try:
+            finance_data = pd.DataFrame(columns=["Date", "Category", "Amount", "Notes"])
+            finance_data.to_csv(db_file, index=False)  # Save the cleared data to CSV
+            st.success("All data cleared!")
+        except Exception as e:
+            st.error("Failed to clear data.")
+            st.write(f"Error details: {e}")
