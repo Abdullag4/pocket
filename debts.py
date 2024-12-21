677390ef1bda20967f5e895842e5d11633398b07
Abdullag4 <abdullakawkas4@gmail.com>
2024-12-21 14:34:22 +0300

    Update debts.py

--- debts.py
+++ debts.py
@@ -26,6 +26,7 @@ def show_debt_page(debt_data, debt_file):
         name = st.text_input("Name", placeholder="Enter the name")
         amount = st.number_input("Amount", min_value=0.0, step=1.0)
         due_date = st.date_input("Due Date")
+        type = st.text_input("Type", placeholder="Enter the reason")
         submitted = st.form_submit_button("Add Debt")
 
         if submitted:
